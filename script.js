982ea2e34e98581c0b9d70238f6f20523f9d5d63
Siddharth Singh <95075464+siddharthsinghvats@users.noreply.github.com>
2022-05-23 19:18:25 +0530

    add volume and interactive progress bar function

--- script.js
+++ script.js
@@ -5,8 +5,84 @@ const next= document.getElementById("next");
 const prev = document.getElementById("prev");
 const nam = document.getElementById("name");
 const singer=document.getElementById("singer");
+const progress = document.getElementById("progress");
+const progressBar = document.getElementById("progress-bar");
+const start = document.getElementById("start");
+const end = document.getElementById("end");
+const barWidth= document.getElementById("progress").clientWidth;
+const volume = document.getElementById("volume");
+const plus= document.getElementById("plus");
+const minus = document.getElementById("minus");
+const toggle = document.getElementById("toggle");
+
+
+let duration;
+music.onloadedmetadata= ()=>{
+    duration = music.duration;
+    // console.log(duration);
+    end.innerText=Math.floor(duration/60)+':'+Math.floor(duration%60);
+}
+
+toggle.addEventListener('click',()=>{
+    let curTitle = toggle.getAttribute('title');
+    if(curTitle==='sound'){
+        toggle.setAttribute('title',"silent");
+        music.volume=1;
+        toggle.classList.remove("silent");
+    }else{
+        toggle.setAttribute('title',"sound");
+        toggle.classList.add("silent");
+        music.volume=0;
+    }
+})
+
+plus.addEventListener('click',()=>{
+    if(music.volume<1){
+        music.volume+=.1;
+    }
+    plus.classList.add("pressed");
+    setTimeout(()=>{
+        plus.classList.remove("pressed");
+    },200)
+})
+minus.addEventListener('click',()=>{
+    if(music.volume>0){
+        music.volume-=.1;
+    }
+    minus.classList.add("pressed");
+    setTimeout(()=>{
+        minus.classList.remove("pressed");
+    },200)
+})
+
+music.addEventListener("timeupdate",()=>{
+    const per=music.currentTime*100/duration;
+    progressBar.style.width=`${per}%`;
+    let sec = Math.floor(music.currentTime%60);
+    if(sec<10) sec= '0'+sec;
+    start.innerText = Math.floor(music.currentTime/60)+':'+sec;
+    if(music.currentTime===duration){
+        next.click();
+    }
+})
+
+
+progress.addEventListener('click',(e)=>{
+    const clickedPos = e.offsetX;
+    const per=Math.round(clickedPos*100/barWidth);
+    progressBar.style.width=`${per}%`;
+    const curDuration = Math.round(per*duration/100);
+    music.currentTime=curDuration;
+    let sec = Math.floor(music.currentTime%60);
+    if(sec<10) sec= '0'+sec;
+    start.innerText = Math.floor(curDuration/60)+':'+sec;
+  
+})
 
 let cur =0;
+
+let curVol=1;
+
 
 
 
@@ -58,7 +134,6 @@ main.addEventListener('click',()=>{
     }
 })
 
-
 next.addEventListener('click',()=>{
     cur++;
     if(cur===songs.length) cur=0;
@@ -90,8 +165,5 @@ prev.addEventListener('click',()=>{
         main.setAttribute('class',"fa fa-solid fa-pause");
         music.play();
         img.setAttribute('class','anime');
-
-    
-
 })
 
